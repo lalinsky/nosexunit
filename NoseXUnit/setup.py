1941c7334aabf7dd4a94b960b41e929d24c250bd
nosexunit <nosexunit@549516f5-ea2c-0410-bc29-f7564ae968c0>
2007-04-05 08:31:54 +0000

    Add dependences on nose >= 0.9.1 and modify script to launch tests from bamboo
    git-svn-id: https://nosexunit.svn.sourceforge.net/svnroot/nosexunit/trunk@5 549516f5-ea2c-0410-bc29-f7564ae968c0

--- NoseXUnit/setup.py
+++ NoseXUnit/setup.py
@@ -4,21 +4,22 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-	name="NoseXUnit",
-	version="0.1",
-	description="XML Output plugin for Nose",
-	long_description="A plugin for nose/nosetests that produces an XML report of the result of a test.",
-	author="Mansion Olivier",
-	author_email="nosexunit@gmail.com",
-	packages=['nosexunit'],
-	classifiers=[
-		'Development Status :: 3 - Alpha',
-		'Intended Audience :: Developers',
-		'Programming Language :: Python',
-		'Topic :: Communications :: Email',
-		'Topic :: Office/Business',
-		'Topic :: Software Development :: Testing',
-		],
-	entry_points = {'nose.plugins': [ 'nosexunit = nosexunit.plugin:NoseXUnit' ] },
-        test_suite = 'nose.collector',
-	)
+    name="NoseXUnit",
+    version="0.1",
+    description="XML Output plugin for Nose",
+    long_description="A plugin for nose/nosetests that produces an XML report of the result of a test.",
+    author="Mansion Olivier",
+    author_email="nosexunit@gmail.com",
+    packages=['nosexunit'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email',
+        'Topic :: Office/Business',
+        'Topic :: Software Development :: Testing',
+        ],
+    install_requires = [ "nose >= 0.9.1", ],
+    entry_points = {'nose.plugins': [ 'nosexunit = nosexunit.plugin:NoseXUnit' ] },
+    test_suite = 'nose.collector',
+)
